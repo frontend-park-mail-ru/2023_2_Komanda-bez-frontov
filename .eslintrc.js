d3dd4d5cc5331dc6f6f89d11640f2568a57cf03d
rissenberg <rissenberg@github.com>
2023-11-06 16:08:36 +0300

    why

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  ignorePatterns: ['*precompiled.js'],
-  rules: {
-    'import/no-cycle': 'off',
-    'import/extensions': 'off',
-    'import/prefer-default-export': 'off',
-    'no-console': 'off',
-    'class-methods-use-this': 'off',
-    'prefer-destructuring': 'off',
-    'object-curly-spacing': 'off',
-    'no-undef': 'off',
-    'guard-for-in': 'off',
-  },
+    extends: 'airbnb-base',
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    ignorePatterns: ['*precompiled.js'],
+    rules: {
+        'import/no-cycle': 'off',
+        'import/extensions': 'off',
+        'import/prefer-default-export': 'off',
+        'no-console': 'off',
+        'class-methods-use-this': 'off',
+        'prefer-destructuring': 'off',
+        'object-curly-spacing': 'off',
+        'no-undef': 'off',
+        'guard-for-in': 'off',
+    },
 };
