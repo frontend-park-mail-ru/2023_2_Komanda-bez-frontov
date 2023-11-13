4a33b21a876ddfc2e9e148e94aac285a637376b1
AlexnadrLarin <allarin03@mail.ru>
2023-11-13 06:59:23 +0300

    Add update profile page

---
+++ public/components/pages/UpdateProfile/update_profile.js
@@ -0,0 +1,53 @@
+import {ROUTES} from '../../../config.js';
+import {removeMessage, renderMessage} from '../../Message/message.js';
+import {goToPage} from '../../../modules/router.js';
+import {STORAGE} from '../../../modules/storage.js';
+
+/**
+ * Функция для рендеринга страницы профиля авторизированного пользователя.
+ * Если пользователь не авторизован, происходит редирект на страницу входа.
+ *
+ * @async
+ * @function
+ * @return {void}
+ */
+export const renderUpdateProfile = async () => {
+  // removeMessage();
+
+  // // Проверка авторизации
+  // if (!STORAGE.user) {
+  //   goToPage(ROUTES.login);
+  //   renderMessage('Вы не авторизованы!', true);
+  //   return;
+  // }
+
+  const rootElement = document.querySelector('#root');
+  rootElement.innerHTML = '';
+  rootElement.innerHTML = Handlebars.templates.update_profile();
+
+  // const user = STORAGE.user;
+  // const profilePicture = document.querySelector('#profile-page-picture');
+  // if (STORAGE.avatar) {
+  //   profilePicture.src = `data:image/png;base64, ${STORAGE.avatar}`;
+  // }
+  // const name = document.querySelector('#profile-page-name');
+  // name.textContent = user.first_name;
+  // const username = document.querySelector('#profile-page-username');
+  // username.textContent = user.username;
+  // const email = document.querySelector('#profile-page-email');
+  // email.textContent = user.email;
+  //
+  // // const settingButton = document.querySelector("#profile-settings-button");
+  // // settingButton.textContent = res.forms[id].title;
+  // // settingButton.addEventListener("click", function (e) {
+  // //     goToPage(ROUTES.settings);
+  // // });
+  // const formsButton = document.querySelector('#profile-forms-button');
+  // formsButton.addEventListener('click', () => {
+  //   goToPage(ROUTES.forms);
+  // });
+  // const historyButton = document.querySelector("#profile-history-button");
+  // historyButton.addEventListener("click", function (e) {
+  //     goToPage(ROUTES.history);
+  // });
+};
